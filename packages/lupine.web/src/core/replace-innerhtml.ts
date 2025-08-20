08c7a18ec6a4ac1f63ed0110cadd4bc0f47168ce
uuware <uuware@gmail.com>
2025-08-20 23:50:40 +1200

    Add loadContent

--- packages/lupine.web/src/core/replace-innerhtml.ts
+++ packages/lupine.web/src/core/replace-innerhtml.ts
@@ -1,4 +1,10 @@
 export const replaceInnerhtml = async (el: Element, newHtml: string) => {
+  // keep <style id="sty-${newProps._id}">...</style>
+  const firstDom = el.firstChild as Element;
+  if (firstDom && firstDom.tagName === 'STYLE') {
+    firstDom.parentNode?.removeChild(firstDom);
+  }
+
   const promises: Promise<void>[] = [];
   el.querySelectorAll('[data-ref]').forEach((child: any) => {
     if (child._lj && child._lj.onUnload) {
@@ -7,4 +13,8 @@ export const replaceInnerhtml = async (el: Element, newHtml: string) => {
   });
   await Promise.all(promises);
   el.innerHTML = newHtml;
+
+  if (firstDom && firstDom.tagName === 'STYLE') {
+    el.insertBefore(firstDom, el.firstChild);
+  }
 };
