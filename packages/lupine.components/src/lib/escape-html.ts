943eb3ae63a4edf3383821b960e19ed7580916fd
uuware <uuware@gmail.com>
2025-12-22 22:00:28 +1300

    Add shell cmd

--- packages/lupine.components/src/lib/escape-html.ts
+++
@@ -1,9 +0,0 @@
-export const escapeHtml = (str: string | number | boolean) => {
-  if (!str || typeof str !== 'string') return str;
-  return str
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;');
-};
