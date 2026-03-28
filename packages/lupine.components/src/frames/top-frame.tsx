671649306bea2ba8668308e1043c7c41782c4ead
uuware <uuware@gmail.com>
2026-03-29 00:30:37 +1300

    Fix top safe area

--- packages/lupine.components/src/frames/top-frame.tsx
+++ packages/lupine.components/src/frames/top-frame.tsx
@@ -16,8 +16,10 @@ export const TopFrame = async (placeholderClassname: string, vnode: VNode<any>) 
       flexDirection: 'column',
       height: '100%',
       // trick: to put two padding-top properties
+    '@supports (-webkit-touch-callout: none)': {
       'padding-top ': 'constant(safe-area-inset-top)',
       'padding-top': 'env(safe-area-inset-top)',
+    },
     },
   };
 
--- packages/lupine.press/src/frames/press-frame.tsx
+++ packages/lupine.press/src/frames/press-frame.tsx
@@ -95,8 +95,10 @@ export const PressFrame = (props: PressFrameProps) => {
       maxWidth: MediaQueryMaxWidth.DesktopMax,
       margin: 'auto',
       // trick: to put two padding-top properties
-      'padding-top ': 'constant(safe-area-inset-top)',
-      'padding-top': 'env(safe-area-inset-top)',
+      '@supports (-webkit-touch-callout: none)': {
+        'padding-top ': 'constant(safe-area-inset-top)',
+        'padding-top': 'env(safe-area-inset-top)',
+      },
     },
     '.press-frame-header': {
       display: 'flex',
