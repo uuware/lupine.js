d6f660824df6cb9746b34d4a33ae1e48f87d81cc
uuware <uuware@gmail.com>
2026-03-29 15:49:44 +1300

    Fix github page path

--- packages/create-lupine/templates/lupine-template-responsive-starter/web/src/frames/app-responsive-frame.tsx
+++ packages/create-lupine/templates/lupine-template-responsive-starter/web/src/frames/app-responsive-frame.tsx
@@ -9,12 +9,18 @@ const getSiteFooter = async () => '© 2026 My Note App';
 // 'sidemenu' | 'tabs', this will be replaced by create-lupine
 const DEFAULT_LAYOUT = 'tabs';
 export const AppResponsiveFrame = async (placeholderClassname: string, vnode: VNode<any>) => {
+  // special login for github pages
+  let subDir = '';
+  if (typeof window !== 'undefined' && window.location.hostname === 'uuware.github.io') {
+    // first section is subDir
+    subDir = '/' + window.location.pathname.split('/')[1];
+  }
   const mobileBottomMenu = [
-    { icon: 'ma-home-outline', url: '/', text: 'Notes' },
-    { icon: 'icon-finance', url: '/finance', text: 'Finance' },
-    { icon: 'ma-book-outline', url: '/about', text: 'About', topout: true },
-    { icon: 'ma-tools', url: '/tools', text: 'Tools' },
-    { icon: 'ma-account-cog-outline', url: '/mine', text: 'Mine' },
+    { icon: 'ma-home-outline', url: subDir + '/', text: 'Notes' },
+    { icon: 'icon-finance', url: subDir + '/finance', text: 'Finance' },
+    { icon: 'ma-book-outline', url: subDir + '/about', text: 'About', topout: true },
+    { icon: 'ma-tools', url: subDir + '/tools', text: 'Tools' },
+    { icon: 'ma-account-cog-outline', url: subDir + '/mine', text: 'Mine' },
   ];
 
   const layout: 'sidemenu' | 'tabs' = (localStorage.getItem('app-layout') as any) || DEFAULT_LAYOUT;
