93c0b4bfe94b0f74f98b16dfabfe71fccfc4900e
uuware <uuware@gmail.com>
2026-07-04 14:03:00 +1200

    Fix evalExpression

--- packages/lupine.api/dev/plugin-ifelse.test.js
+++ packages/lupine.api/dev/plugin-ifelse.test.js
@@ -1,37 +1,87 @@
-/*
-(async () => {
-    var vars = {
-        TEST: undefined,
-        TEST1: '', // false
-        TEST2: '2',
-        PROD: false,
-    }
-      const x = await processOneFile(vars, 'test.js');
-  console.log(x);
-  process.exit(0);
-})();
-*/
-console.log('test start');
-console.log('test start');
-// #if TEST
-console.log('test');
-// #elseif PROD
-console.log('prod');
-// #else
-console.log('not test');
-// #endif
-console.log('test end');
-console.log('test end');
-// #if TEST1 && TEST2
-console.log('test1 and test2');
-// #endif
+import test from 'node:test';
+import assert from 'node:assert';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import os from 'node:os';
+import { createRequire } from 'node:module';
 
-// #if TEST1 || TEST2
-console.log('test1 or test2');
-// #endif
+// Use createRequire to import the CommonJS module in an ESM environment
+const require = createRequire(import.meta.url);
+const plugin = require('./plugin-ifelse.js');
+const { processOneFile } = plugin._test;
 
-// #if TEST2
-console.log('test2');
-// #endif
-console.log('test end');
-console.log('test end');
+async function runTestCode(vars, code) {
+  const tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'ifelse-test-'));
+  const fpath = path.join(tmpDir, 'test.js');
+  await fs.writeFile(fpath, code, 'utf8');
+  const result = await processOneFile(vars, fpath);
+  await fs.rm(tmpDir, { recursive: true, force: true });
+  return result;
+}
+
+test('Plugin If-Else Compilation', async (t) => {
+  
+  await t.test('1. Basic //#if (no spaces)', async () => {
+    const code = `
+//#if MOBILE
+console.log('is mobile');
+//#endif
+    `;
+    
+    let res = await runTestCode({ MOBILE: '1' }, code);
+    assert.match(res, /is mobile/, "Should keep code when MOBILE is '1'");
+
+    res = await runTestCode({ MOBILE: '' }, code);
+    assert.doesNotMatch(res, /is mobile/, "Should remove code when MOBILE is ''");
+
+    res = await runTestCode({}, code);
+    assert.doesNotMatch(res, /is mobile/, "Should remove code when MOBILE is undefined");
+  });
+
+  await t.test('2. Spaces inside comment //   #if   xxx', async () => {
+    const code = `
+//   #if   MOBILE
+console.log('is mobile');
+//   #endif
+    `;
+    
+    let res = await runTestCode({ MOBILE: '1' }, code);
+    assert.match(res, /is mobile/, "Should keep code when MOBILE is '1'");
+
+    res = await runTestCode({ MOBILE: '' }, code);
+    assert.doesNotMatch(res, /is mobile/, "Should remove code when MOBILE is ''");
+  });
+
+  await t.test('3. Indented comment    //#if xxx', async () => {
+    const code = `
+function test() {
+    //#if MOBILE
+    console.log('is mobile');
+    //#endif
+}
+    `;
+    
+    let res = await runTestCode({ MOBILE: '1' }, code);
+    assert.match(res, /is mobile/, "Should keep code when MOBILE is '1'");
+
+    res = await runTestCode({ MOBILE: '' }, code);
+    assert.doesNotMatch(res, /is mobile/, "Should remove code when MOBILE is ''");
+  });
+
+  await t.test('4. Negative condition !xxx when undefined', async () => {
+    const code = `
+//#if !MOBILE
+console.log('is not mobile');
+//#endif
+    `;
+    
+    // If MOBILE is not defined, !MOBILE evaluates to true
+    let res = await runTestCode({}, code);
+    assert.match(res, /is not mobile/, "Should keep code when MOBILE is undefined and using !MOBILE");
+
+    // If MOBILE is '1', !MOBILE evaluates to false
+    res = await runTestCode({ MOBILE: '1' }, code);
+    assert.doesNotMatch(res, /is not mobile/, "Should remove code when MOBILE is '1' and using !MOBILE");
+  });
+});
+
