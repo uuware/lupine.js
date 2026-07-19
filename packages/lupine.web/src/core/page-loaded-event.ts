d9f16aeea380197f48a1df267ccff5c4e6e202ec
uuware <uuware@gmail.com>
2026-07-19 21:57:52 +1200

    Rename

--- packages/lupine.web/src/core/page-loaded-event.ts
+++ packages/lupine.web/src/core/page-loaded-event.ts
@@ -1,21 +1,21 @@
-// some events need to be done before the page is rendered
-export type PageLoadedEventProps = () => void;
-const _pageLoadedEvents: PageLoadedEventProps[] = [];
-export const callPageLoadedEvent = () => {
-  _pageLoadedEvents.forEach((i) => {
-    try {
-      i();
-    } catch (e) {
-      console.error(e);
-    }
-  });
+// some events need to be done after the dom is loaded
+export type DomLoadedEventProps = () => void;
+const _domLoadedEvents: DomLoadedEventProps[] = [];
+export const callDomLoadedEvent = () => {
+  if (typeof document !== 'undefined') {
+    _domLoadedEvents.forEach((i) => {
+      try {
+        i();
+      } catch (e) {
+        console.error(e);
+      }
+    });
+  }
 };
 
-// The fn is called when the page is first time loaded
-export const bindPageLoadedEvent = (fn: PageLoadedEventProps) => {
-  if (typeof document === 'undefined') {
-    throw new Error('bindPageLoadedEvent can only be called in the browser');
+// Not for SSR: This fn is called when the dom is first time loaded
+export const bindDomLoadedEvent = (fn: DomLoadedEventProps) => {
+  if (typeof document !== 'undefined') {
+    _domLoadedEvents.push(fn);
   }
-
-  _pageLoadedEvents.push(fn);
 };
